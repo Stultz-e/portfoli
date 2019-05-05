c24284c3d8d148af3bbec526d32b23f1c8beab35
cstultz87 <cstultz@oswego.edu>
2019-05-04 22:57:20 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+watch = require('gulp-watch')
 
 gulp.task('default', function() {
     console.log("hurray - you did a thing");
@@ -8,3 +9,13 @@ gulp.task('html', function() {
     console.log("Imagine a gouch");
 }); 
 
+gulp.task('styles', function() {
+    console.log("Imagine a gouch sass shit");
+}); 
+
+gulp.task('watch', function() {
+    
+    watch('./app/index.html', function() {
+        gulp.src('./app/assets/styles/styles.css')
+    });
+})
