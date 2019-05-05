ae054b43d4115fb83a8ee172078304c042ae2d6e
cstultz87 <cstultz@oswego.edu>
2019-05-04 22:40:59 -0500

    d

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    console.log("hurray - you did a thing");
+});
+
+gulp.task('html', function() {
+    console.log("Imagine a gouch");
+}); 
+
